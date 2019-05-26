0f000b723d3a1fe6e2b5b69da198d68289820d3b
Yuria <nishimura.yuria@gmail.com>
2019-05-26 16:49:59 -0700

    feat(index): format text

--- index.ts
+++ index.ts
@@ -4,20 +4,18 @@ import axios from "axios";
 dotenv.config();
 const url = process.env.SLACK_WEBHOOK_URL;
 
-try {
-  (async () => {
-    const text = "hello leetcode bot!";
-    console.log(text);
+(async () => {
+  const title = "860. Lemonade Change";
+  const difficulty = "Easy";
+  const link = "https://leetcode.com/problems/lemonade-change/";
+  const text = `${title} - ${difficulty}\n${link}`;
 
-    const response = await axios({
-      method: "post",
-      url,
-      data: {
-        text
-      }
-    });
-    console.log(response);
-  })();
-} catch (err) {
-  throw err;
-}
+  const response = await axios({
+    method: "post",
+    url,
+    data: {
+      text
+    }
+  });
+  console.log(response.status, response.config.data);
+})();
