9f339ed9b2ff93ee033b49564671b29ca3471691
Yuria <nishimura.yuria@gmail.com>
2019-05-26 18:50:54 -0700

    chore(package): add prettier rules

--- index.ts
+++ index.ts
@@ -1,21 +1,21 @@
-import * as dotenv from "dotenv";
-import axios from "axios";
+import * as dotenv from 'dotenv';
+import axios from 'axios';
 
 dotenv.config();
 const url = process.env.SLACK_WEBHOOK_URL;
 
 (async () => {
-  const title = "860. Lemonade Change";
-  const difficulty = "Easy";
-  const link = "https://leetcode.com/problems/lemonade-change/";
+  const title = '860. Lemonade Change';
+  const difficulty = 'Easy';
+  const link = 'https://leetcode.com/problems/lemonade-change/';
   const text = `${title} - ${difficulty}\n${link}`;
 
   const response = await axios({
-    method: "post",
+    method: 'post',
     url,
     data: {
-      text
-    }
+      text,
+    },
   });
   console.log(response.status, response.config.data);
 })();
